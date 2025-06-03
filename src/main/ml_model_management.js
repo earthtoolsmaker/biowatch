611e5ebc9897f34e69d963622f2926914bf03882
Arthur Caillau <arthur@caillau.me>
2025-06-03 19:23:50 +0200

    chore: use resources path for prod for scriptPath

--- src/main/ml_model_management.js
+++ src/main/ml_model_management.js
@@ -433,11 +433,14 @@ async function startSpeciesNetHTTPServer({
     pythonEnvironment.reference.id
   )
   log.info('Local Python Environment root dir is', localInstalRootDirPythonEnvironment)
-  // FIXME: this works for the dev environment but not for prod.
-  // Make sure the build steps include the python script as a resource in the electron buil
+  // TODO: Make sure the build steps include the python script as a resource in the electron buil
   // use app.getResource to target the python script
-  const scriptPath = join(__dirname, '../../python-environments/common/run_speciesnet_server.py')
-  const pythonInterpreter = join(localInstalRootDirPythonEnvironment, 'bin', 'python')
+  const scriptPath = is.dev
+    ? join(__dirname, '../../python-environments/common/run_speciesnet_server.py')
+    : join(process.resourcesPath, 'python-environments', 'common', 'run_speciesnet_server.py')
+  const pythonInterpreter = is.dev
+    ? join(__dirname, '../../python-environments/common/.venv/bin/python')
+    : join(localInstalRootDirPythonEnvironment, 'bin', 'python')
   log.info('Python Interpreter found in', pythonInterpreter)
   log.info('Script path is', scriptPath)
   const scriptArgs = [
