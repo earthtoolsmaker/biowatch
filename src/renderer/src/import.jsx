15df4e798a7ecd124399d39232756e617322ba17
Jeremy Vuillermet <jeremy.vuillermet@gmail.com>
2026-01-15 13:05:13 +0100

    unused

--- src/renderer/src/import.jsx
+++ src/renderer/src/import.jsx
@@ -393,7 +393,7 @@ export default function Import({ onNewStudy }) {
   }
 
   return (
-    <div className="h-full overflow-y-auto bg-popover">
+    <div className="h-full overflow-y-auto">
       <div className="max-w-4xl mx-auto p-8">
         {/* Header */}
         <div className="mb-10">
